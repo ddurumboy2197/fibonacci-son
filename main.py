04653f27ee4d0119706024f34c11fc418d479246
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-13 16:27:24 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,33 @@
+def ekub(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+
+print("EKUB:", ekub(a, b))
+```
+
+```python
+def ekub(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+
+print("EKUB:", ekub(a, b))
+```
+
+```python
+def ekub(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+
+print("EKUB:", ekub(a, b))
